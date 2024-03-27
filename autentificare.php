680a1526ba059e685975387bfdeb24d185c012d9
Antoci Veaceslav <antoci.slavic2001@gmail.com>
2024-03-27 11:19:56 +0300

    sesiune la autentificare

--- autentificare.php
+++ autentificare.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 // Verificăm dacă s-a trimis formularul
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Conectare la baza de date (modificați detaliile corespunzătoare)
@@ -14,12 +14,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Verificăm dacă s-a găsit un rând în baza de date (utilizatorul există)
     if (pg_num_rows($result) == 1) {
-        // Utilizatorul există, redirecționăm către pagina de start sau orice altă pagină dorită
+        // Începem sesiunea
+        session_start();
+
+        // Stocăm detaliile de autentificare în variabile de sesiune
+        $_SESSION['logged_in'] = true;
+        $_SESSION['email'] = $email;
+
+        // Redirectăm către pagina de start sau orice altă pagină dorită
         header("Location: user.php");
         exit;
     } else {
         // Utilizatorul nu există sau parola este incorectă
-        $error_message = "Email sau parolă incorecte. Te rugăm să încerci din nou.";
+        session_start();
+
+        $_SESSION['error_message'] = "Email sau parolă incorecte. Te rugăm să încerci din nou.";
+
+        header("Location: autorizare.php");
     }
 
     // Închidem conexiunea la baza de date
